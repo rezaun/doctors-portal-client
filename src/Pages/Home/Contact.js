e388605cf3870f31cfa89b453877f2e79e2eb927
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-10-10 02:05:54 +0600

    home cotnact form done

---
+++ src/Pages/Home/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import contact from '../../assets/images/appointment.png'
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const Contact = () => {
+    return (
+        <section style={{
+            background: `url(${contact})`
+        }} className='flex justify-center items-center'>
+            <div className='py-28 '>
+                <h4 className='text-primary'>Contact Us</h4>
+                <h2 className='text-3xl text-white'>Stay connected with us</h2>
+                <form action="">
+                    <input type="text" placeholder="Email Address" class="input input-bordered w-full max-w-xs mt-5 flex" />
+                    <input type="text" placeholder="Subject" class="input input-bordered w-full max-w-xs mt-5 flex" />
+                    <textarea class="textarea w-full mt-5 py-10" placeholder="Your Message"></textarea>
+                    <PrimaryButton>Submit</PrimaryButton>
+                </form>
+
+            </div>
+
+
+
+        </section>
+    );
+};
+
+export default Contact;
